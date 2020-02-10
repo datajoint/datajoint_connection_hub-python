e2241ba071de6c49d85a5dc90e3b6ea4adb8498f
guzman-raphael <raphael.h.guzman@gmail.com>
2020-02-10 14:44:30 -0600

    Add working local version.

---
+++ serve.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+up() {
+    git clone -b update_schema --single-branch git@github.com:guzman-raphael/djhub.git
+    if [ "$1" = "LOCAL" ]; then
+        env $(cat LNX.env;echo;cat common.env;echo;cat private.env) \
+            docker-compose -f local-docker-compose.yml up --build
+    elif [ "$1" = "TRAVIS" ]; then
+        env $(cat LNX.env;echo;cat common.env) \
+            docker-compose -f LNX-docker-compose.yml up --build --exit-code-from app
+    fi
+}
+
+down() {
+    if [ "$1" = "LOCAL" ]; then
+        env $(cat LNX.env;echo;cat common.env;echo;cat private.env) \
+            docker-compose -f local-docker-compose.yml down
+    elif [ "$1" = "TRAVIS" ]; then
+        env $(cat LNX.env;echo;cat common.env) \
+            docker-compose -f LNX-docker-compose.yml down
+    fi
+    sudo rm -R djhub
+}
+
+"$@"
--- tests/__init__.py
+++ tests/__init__.py
@@ -5,3 +5,18 @@ import importlib
 import datajoint
 importlib.reload(sys.modules['datajoint'])
 import datajoint as dj
+from hubapi.schema import *
+from os import environ
+
+
+def setup_package():
+    Org.insert1(['datajoint'])
+    db_inst = '{}:3306'.format(environ.get('DJ_HOST', 'fakeservices.datajoint.io'))
+    DatabaseInstance.insert1([db_inst])
+    Project.insert1(['datajoint', 'travis', db_inst])
+
+
+def teardown_package():
+    Project.delete_quick()
+    DatabaseInstance.delete_quick()
+    Org.delete_quick()
